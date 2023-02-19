cd6286597818c610050b4ea412ad6991b16015bd
Erfan Mostafiz <erfan.mostafiz2@gmail.com>
2023-02-20 04:07:57 +0600

    Started Working with Front End - Overlay Navbar built

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from "./component/layout/header/Header.js";
+import {BrowserRouter as Router} from "react-router-dom";
+import WebFont from "webfontloader";
+import React from "react";
+
+function App() {
+
+  React.useEffect(()=>{
+
+    WebFont.load({
+      google:{
+        families: ["Roboto","Droid Sans", "Chilanka"]
+      }
+    })
+  
+  }, [])
+  
+  return <Router>
+      <Header />
+  </Router> ;
+}
+
+export default App;
